e6c890c462d23d0cf0060da80a51ddf9b914833f
Cosmin <cosminzorr@gmail.com>
2018-02-11 12:40:23 +0200

    no msg

--- motors.py
+++ motors.py
@@ -18,19 +18,22 @@ motors.toggleMotors("on")
 # loop over the frames from the video stream
 direction = -100
 
-while True:
-	if direction < -10:
-		motors.move_motors(0, 35, "forward")
-	elif direction > 10:
-		motors.move_motors(35, 0, "forward")				
-	else:
-		motors.move_motors(35, 35, "forward")	
+try:
+	while True:
+		# if direction < -10:
+		# 	motors.move_motors(0, 35, "forward")
+		# elif direction > 10:
+		# 	motors.move_motors(35, 0, "forward")				
+		# else:
+		# 	motors.move_motors(35, 35, "forward")	
 
-	key = cv2.waitKey(1) & 0xFF
+		key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+except KeyboardInterrupt:
+	motors.cleanup_pins()
+
  
 # do a bit of cleanup
-motors.cleanup_pins()
