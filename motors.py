ce8b7734f623a0aefcfadda8d21909aab60d11ec
Cosmin <cosminzorr@gmail.com>
2018-02-11 12:53:19 +0200

    no msg

--- motors.py
+++ motors.py
@@ -20,7 +20,7 @@ ok = 1
 try:
 	while True:
 		if ok == 1:
-			motors.move_motors(0, 100, "forward")
+			motors.move_motors(0, 1, "forward")
 			ok = 0
 except KeyboardInterrupt:
 	motors.cleanup_pins()
