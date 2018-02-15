4f67ada28e7e87b208bcaa2cce33d6f1582c1cbd
Cosmin <cosminzorr@gmail.com>
2018-02-15 19:49:15 +0200

    no msg

--- classes/ServoNew.py
+++ classes/ServoNew.py
@@ -18,7 +18,7 @@ class ServoNew:
     
     def change(self, value):
         print 'Servo to ' + str(value)
-        time.sleep(0.5)
+        time.sleep(0.2)
         # self.pi.set_servo_pulsewidth(self.PIN, value)
         # self.pi.get_servo_pulsewidth(self.PIN, value)
 
