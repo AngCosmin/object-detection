9b369594999b3a2509bed10db994b155463b7bbb
Cosmin <cosminzorr@gmail.com>
2018-02-16 15:13:34 +0200

    no msg

--- demo.py
+++ demo.py
@@ -126,35 +126,35 @@ while True:
 			# 	text += "LEFT 30 RIGHT 30"
 			# 	motors.move_motors(100, 100)
 
-			if abs(lastY - verticaly_object_position) > 20:
-				if verticaly_object_position - lastY < 0:
-					servoValue = servoValue - abs(lastY - verticaly_object_position) * 1000 / height / 2
-					print 'Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)					
-					if servoValue > 1000:
-						servoValue = 1000
-					if servoValue < 2000:
-						servoValue = 2000
-					servo.change(servoValue)
-				else:
-					servoValue = servoValue + abs(verticaly_object_position - lastY) * 1000 / height / 2			
-					print 'Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)
-					if servoValue > 1000:
-						servoValue = 1000
-					if servoValue < 2000:
-						servoValue = 2000
-					servo.change(servoValue)
-				lastY = verticaly_object_position				
+			# if abs(lastY - verticaly_object_position) > 20:
+			# 	if verticaly_object_position - lastY < 0:
+			# 		servoValue = servoValue - abs(lastY - verticaly_object_position) * 1000 / height / 2
+			# 		print 'Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)					
+			# 		if servoValue > 1000:
+			# 			servoValue = 1000
+			# 		if servoValue < 2000:
+			# 			servoValue = 2000
+			# 		servo.change(servoValue)
+			# 	else:
+			# 		servoValue = servoValue + abs(verticaly_object_position - lastY) * 1000 / height / 2			
+			# 		print 'Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)
+			# 		if servoValue > 1000:
+			# 			servoValue = 1000
+			# 		if servoValue < 2000:
+			# 			servoValue = 2000
+			# 		servo.change(servoValue)
+			# 	lastY = verticaly_object_position				
 
-			# if verticaly_object_position < -50: 
-			# 	servoValue -= 25
-			# 	if servoValue < 1000:
-			# 		servoValue = 1000
-			# 	servo.change(servoValue)
-			# elif verticaly_object_position > 50:
-			# 	servoValue += 25
-			# 	if servoValue > 2000:
-			# 		servoValue = 2000
-			# 	servo.change(servoValue)
+			if verticaly_object_position < -50: 
+				servoValue -= 25
+				if servoValue < 1000:
+					servoValue = 1000
+				servo.change(servoValue)
+			elif verticaly_object_position > 50:
+				servoValue += 25
+				if servoValue > 2000:
+					servoValue = 2000
+				servo.change(servoValue)
 
 			text += "Servo value: " + str(servoValue) + " Y: " + str(verticaly_object_position)
 
