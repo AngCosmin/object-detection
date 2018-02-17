2cf88cc9fc016c4af40b2e78db5f698aaf1f14ee
Cosmin <cosminzorr@gmail.com>
2018-02-17 23:50:13 +0200

    no msg

--- demo.py
+++ demo.py
@@ -36,119 +36,120 @@ lastActiveTime = 0
 movingTime = None
 direction = None
 
-try: 
-	# loop over the frames from the video stream
-	while True:
-		frame, mask = camera.compute()
-
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-			# only proceed if the radius meets a minimum size
-			if radius > 7:
-				# draw the circle and centroid on the frame,
-				cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-				cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-				horizontaly_object_position = int(x) - width / 2
-				object_y = int(y) - height / 2
-
-				# write center coords on the screen
-				text = " X: " +  str(int(x)) + " Y: " + str(int(y)) + " HORIZ: " + str(horizontaly_object_position)
-
-				# update the last active time
-				lastActiveTime = time.time()
-
-				# if horizontaly_object_position > 15:
-				# 	# Object on the right side of the image
-
-				# 	object_position_percentage = float(horizontaly_object_position) / (width / 2) * 100
-
-				# 	if object_position_percentage <= 50:
-				# 		if object_position_percentage < 10:
-				# 			object_position_percentage = 10
-
-				# 		text += " LEFT " + str(object_position_percentage * 2) + " RIGHT 0"
-				# 		motors.move_motors(object_position_percentage * 2, 0)	
-				# 	else:
-				# 		if object_position_percentage - 50 < 10:
-				# 			object_position_percentage = 50 + 10
-
-				# 		text += " LEFT 100 RIGHT " + str(-(object_position_percentage - 50) * 2)
-				# 		motors.move_motors(100, -(object_position_percentage - 50) * 2)	
-				# elif horizontaly_object_position < -15:
-				# 	object_position_percentage = -float(horizontaly_object_position) / (width / 2) * 100
-
-				# 	if object_position_percentage <= 50:
-				# 		if object_position_percentage < 10:
-				# 			object_position_percentage = 10
-
-				# 		text += " LEFT 0 RIGHT " + str(object_position_percentage * 2)  
-				# 		motors.move_motors(0, object_position_percentage * 2)	
-				# 	else:
-				# 		if object_position_percentage - 50 < 10:
-				# 			object_position_percentage = 50 + 10
-							
-				# 		text += " LEFT " + str(-(object_position_percentage - 50) * 2) + " RIGHT 100"
-				# 		motors.move_motors(-(object_position_percentage - 50) * 2, 100)
-				# else:
-				# 	text += "LEFT 30 RIGHT 30"
-				# 	motors.move_motors(100, 100)
-
-
-				servo.compute(object_y)
-				
-				cv2.putText(frame, text, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1) #Draw the text
-		else:
-			# if time.time() - lastActiveTime > 10:
-			# 	# He stayed for 10 seconds
-
-			# 	if movingTime == None:
-			# 		# How much time to move ( 2 sec )
-			# 		movingTime = time.time() + 2
-			# 		# Choose a random direction to move
-			# 		direction = random.choice(['left', 'right'])
-			# 	else:
-			# 		if movingTime - time.time() > 0:
-			# 			if direction == 'left':
-			# 				motors.move_motors(-100, 100)
-			# 			else:
-			# 				motors.move_motors(100, -100)							
-			# 		else:
-			# 			movingTime = None
-			# 			lastActiveTime = time.time()
-			# else:
-			# 	motors.stop()
-			motors.stop()
-
-		# show the frame
-		cv2.imshow("Frame", frame)    
-		cv2.imshow("Mask", mask)
-		
-		key = cv2.waitKey(1) & 0xFF
-
-		if key == ord("q"):
-			break		
-
-except Exception as e: 
-	print e
-	clean()
-finally:
-	clean()
-
 def clean():
 	motors.clean()
 	relay.clean()
 	servo.clean()
 	camera.clean()
+
+if __name__ == "__main__":
+	try: 
+		# loop over the frames from the video stream
+		while True:
+			frame, mask = camera.compute()
+
+			# find contours in the mask and initialize the current
+			# (x, y) center of the ball
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+			# only proceed if at least one contour was found
+			if len(cnts) > 0:
+				# find the largest contour in the mask, then use
+				# it to compute the minimum enclosing circle and
+				# centroid
+				c = max(cnts, key=cv2.contourArea)
+				((x, y), radius) = cv2.minEnclosingCircle(c)
+				M = cv2.moments(c)
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+				# only proceed if the radius meets a minimum size
+				if radius > 7:
+					# draw the circle and centroid on the frame,
+					cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+					cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+					horizontaly_object_position = int(x) - width / 2
+					object_y = int(y) - height / 2
+
+					# write center coords on the screen
+					text = " X: " +  str(int(x)) + " Y: " + str(int(y)) + " HORIZ: " + str(horizontaly_object_position)
+
+					# update the last active time
+					lastActiveTime = time.time()
+
+					# if horizontaly_object_position > 15:
+					# 	# Object on the right side of the image
+
+					# 	object_position_percentage = float(horizontaly_object_position) / (width / 2) * 100
+
+					# 	if object_position_percentage <= 50:
+					# 		if object_position_percentage < 10:
+					# 			object_position_percentage = 10
+
+					# 		text += " LEFT " + str(object_position_percentage * 2) + " RIGHT 0"
+					# 		motors.move_motors(object_position_percentage * 2, 0)	
+					# 	else:
+					# 		if object_position_percentage - 50 < 10:
+					# 			object_position_percentage = 50 + 10
+
+					# 		text += " LEFT 100 RIGHT " + str(-(object_position_percentage - 50) * 2)
+					# 		motors.move_motors(100, -(object_position_percentage - 50) * 2)	
+					# elif horizontaly_object_position < -15:
+					# 	object_position_percentage = -float(horizontaly_object_position) / (width / 2) * 100
+
+					# 	if object_position_percentage <= 50:
+					# 		if object_position_percentage < 10:
+					# 			object_position_percentage = 10
+
+					# 		text += " LEFT 0 RIGHT " + str(object_position_percentage * 2)  
+					# 		motors.move_motors(0, object_position_percentage * 2)	
+					# 	else:
+					# 		if object_position_percentage - 50 < 10:
+					# 			object_position_percentage = 50 + 10
+								
+					# 		text += " LEFT " + str(-(object_position_percentage - 50) * 2) + " RIGHT 100"
+					# 		motors.move_motors(-(object_position_percentage - 50) * 2, 100)
+					# else:
+					# 	text += "LEFT 30 RIGHT 30"
+					# 	motors.move_motors(100, 100)
+
+
+					servo.compute(object_y)
+					
+					cv2.putText(frame, text, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1) #Draw the text
+			else:
+				# if time.time() - lastActiveTime > 10:
+				# 	# He stayed for 10 seconds
+
+				# 	if movingTime == None:
+				# 		# How much time to move ( 2 sec )
+				# 		movingTime = time.time() + 2
+				# 		# Choose a random direction to move
+				# 		direction = random.choice(['left', 'right'])
+				# 	else:
+				# 		if movingTime - time.time() > 0:
+				# 			if direction == 'left':
+				# 				motors.move_motors(-100, 100)
+				# 			else:
+				# 				motors.move_motors(100, -100)							
+				# 		else:
+				# 			movingTime = None
+				# 			lastActiveTime = time.time()
+				# else:
+				# 	motors.stop()
+				motors.stop()
+
+			# show the frame
+			cv2.imshow("Frame", frame)    
+			cv2.imshow("Mask", mask)
+			
+			key = cv2.waitKey(1) & 0xFF
+
+			if key == ord("q"):
+				break		
+
+	except Exception as e: 
+		print e
+		clean()
+	finally:
+		clean()
