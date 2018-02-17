b9911f0a27678a23d313f8f21652de90e4e6c214
Cosmin <cosminzorr@gmail.com>
2018-02-18 00:32:06 +0200

    no msg

--- classes/Camera.py
+++ classes/Camera.py
@@ -48,8 +48,18 @@ class Camera:
                 cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
+                if frame == None:
+                    print 'Frame none'
+                if mask == None:
+                    print 'Mask none'
+
                 return frame, mask, x, y
         else:
+            if frame == None:
+                print 'Frame none'
+            if mask == None:
+            print 'Mask none'
+
             return frame, mask, sys.maxint, sys.maxint
 
     def clean(self):
