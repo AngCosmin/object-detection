057a7c8c9a020f55d49f04b021574af74c11d150
Cosmin <cosminzorr@gmail.com>
2018-02-17 22:07:04 +0200

    no msg

--- classes/Camera.py
+++ classes/Camera.py
@@ -4,6 +4,7 @@ import imutils
 class Camera:
     def __init__(self, usePiCamera=True):
         self.camera = VideoStream(usePiCamera).start()
+        return self.camera
 
     def read(self):
         return self.camera.read()
