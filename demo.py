3da9dca68e8721f3bb347fc249d6bc91b8092708
Cosmin <cosminzorr@gmail.com>
2018-02-17 13:46:38 +0200

    no msg

--- demo.py
+++ demo.py
@@ -132,31 +132,31 @@ try:
 					text += "LEFT 30 RIGHT 30"
 					motors.move_motors(100, 100)
 
-				# if abs(verticaly_object_position - lastY) > 30:
-				# 	# if verticaly_object_position - lastY < 0:
-				# 	servoValue = servoValue + (verticaly_object_position - lastY) * 500 / height
+				if abs(verticaly_object_position - lastY) > 30:
+					# if verticaly_object_position - lastY < 0:
+					servoValue = servoValue + (verticaly_object_position - lastY) * 500 / height
 
-				# 	print '[IF] Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)					
+					print '[IF] Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)					
 
+					servo.change(servoValue)
+					# else:
+					# 	servoValue = servoValue + abs(verticaly_object_position - lastY) * 1000 / height / 2
+
+					# 	print '[ELSE] Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)
+
+					# 	servo.change(servoValue)
+					lastY = verticaly_object_position						
+
+				# if verticaly_object_position < -50: 
+				# 	servoValue -= 25
+				# 	if servoValue < 1000:
+				# 		servoValue = 1000
 				# 	servo.change(servoValue)
-				# 	# else:
-				# 	# 	servoValue = servoValue + abs(verticaly_object_position - lastY) * 1000 / height / 2
-
-				# 	# 	print '[ELSE] Servo to ' + str(servoValue) + ' Last Y: ' + str(lastY) + ' Vectical object: ' + str(verticaly_object_position)
-
-				# 	# 	servo.change(servoValue)
-				# 	lastY = verticaly_object_position						
-
-				if verticaly_object_position < -50: 
-					servoValue -= 25
-					if servoValue < 1000:
-						servoValue = 1000
-					servo.change(servoValue)
-				elif verticaly_object_position > 50:
-					servoValue += 25
-					if servoValue > 2000:
-						servoValue = 2000
-					servo.change(servoValue)
+				# elif verticaly_object_position > 50:
+				# 	servoValue += 25
+				# 	if servoValue > 2000:
+				# 		servoValue = 2000
+				# 	servo.change(servoValue)
 
 				text += "Servo value: " + str(servoValue) + " Y: " + str(verticaly_object_position)
 
