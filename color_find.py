9557fd416455cbde23fb90e10558fc8298460a4b
Cosmin <cosminzorr@gmail.com>
2018-02-17 12:04:22 +0200

    no msg

--- color_find.py
+++ color_find.py
@@ -1,107 +1,157 @@
 #!/usr/bin/env python
 
-# usage $ python click_and_crop.py --image sample.jpg
 # import the necessary packages
-import argparse
-import cv2
+from imutils.video import VideoStream
 import numpy as np
- 
+import imutils
+import cv2
+import argparse
+
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
 x_start, y_start, x_end, y_end = 0, 0, 0, 0
 cropping = False
 getROI = False
 refPt = []
+lower = np.array([])
+upper = np.array([])
 
-# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
+ap.add_argument("-p", "--picamera", type=int, default=-1, help="whether or not the Raspberry Pi camera should be used")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
 args = vars(ap.parse_args())
- 
-# load the image, clone it, and setup the mouse callback function
-image = cv2.imread(args["image"])
-clone = image.copy()
 
+camera = VideoStream(usePiCamera=args["picamera"] > 0).start()
 
 def click_and_crop(event, x, y, flags, param):
-	# grab references to the global variables
-	global x_start, y_start, x_end, y_end, cropping, getROI
+    # grab references to the global variables
+    global x_start, y_start, x_end, y_end, cropping, getROI
 
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
-	if event == cv2.EVENT_LBUTTONDOWN:
-		x_start, y_start, x_end, y_end = x, y, x, y
-		cropping = True
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being
+    # performed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_start, y_start, x_end, y_end = x, y, x, y
+        cropping = True
 
-	elif event == cv2.EVENT_MOUSEMOVE:
-		if cropping == True:
-			x_end, y_end = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if cropping == True:
+            x_end, y_end = x, y
 
-	# check to see if the left mouse button was released
-	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
-		x_end, y_end = x, y
-		cropping = False
-		getROI = True
+    # check to see if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates and indicate that
+        # the cropping operation is finished
+        x_end, y_end = x, y
+        cropping = False
+        getROI = True
 
- 
+
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", click_and_crop)
- 
-# keep looping until the 'q' key is pressed
+    
+# keep looping
 while True:
 
-	i = image.copy()
+    if not getROI:
 
-	if not cropping and not getROI:
-		cv2.imshow("image", image)
+        while True:
+            # grab the current frame
+            (grabbed, frame) = camera.read()
 
-	elif cropping and not getROI:
-		cv2.rectangle(i, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
-		cv2.imshow("image", i)
+            if not grabbed:
+                break
 
-	elif not cropping and getROI:
-		cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
-		cv2.imshow("image", image)
+            if not cropping and not getROI:
+                cv2.imshow("image", frame)
 
-	key = cv2.waitKey(1) & 0xFF
+            elif cropping and not getROI:
+                cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+                cv2.imshow("image", frame)
+
+            elif not cropping and getROI:
+                cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+                cv2.imshow("image", frame)
+                break
+
+            key = cv2.waitKey(1) & 0xFF
+            # if the 'q' key is pressed, stop the loop
+            if key == ord("q"):
+            	noROI = True
+                break
+
+        # if there are two reference points, then crop the region of interest
+        # from teh image and display it
+        refPt = [(x_start, y_start), (x_end, y_end)]
+
+        roi = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+        #cv2.imshow("ROI", roi)
+
+        hsvRoi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        print('min H = {}, min S = {}, min V = {}; max H = {}, max S = {}, max V = {}'.format(hsvRoi[:,:,0].min(), hsvRoi[:,:,1].min(), hsvRoi[:,:,2].min(), hsvRoi[:,:,0].max(), hsvRoi[:,:,1].max(), hsvRoi[:,:,2].max()))
+     
+        lower = np.array([hsvRoi[:,:,0].min(), hsvRoi[:,:,1].min(), hsvRoi[:,:,2].min()])
+        upper = np.array([hsvRoi[:,:,0].max(), hsvRoi[:,:,1].max(), hsvRoi[:,:,2].max()])
+
+
+
+
+
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+
+    if not grabbed:
+        break
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    #frame = imutils.resize(frame, width=600)
+
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color from dictionary`1, then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    kernel = np.ones((9,9),np.uint8)
+    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+            
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    
+        # only proceed if the radius meets a minimum size. Correct this value for your obect's size
+        if radius > 0.5:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
+            cv2.putText(frame, 'center: {}, {}'.format(int(x), int(y)), (int(x-radius),int(y-radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
+
+     
+    # show the frame to our screen
+    cv2.imshow("image", frame)
+    
+    key = cv2.waitKey(1) & 0xFF
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+    elif key == ord("r"):
+        getROI = False
  
-	# if the 'r' key is pressed, reset the cropping region
-	if key == ord("r"):
-		image = clone.copy()
-		getROI = False
- 
-	# if the 'c' key is pressed, break from the loop
-	elif key == ord("c"):
-		break
-
-
- 
-# if there are two reference points, then crop the region of interest
-# from teh image and display it
-refPt = [(x_start, y_start), (x_end, y_end)]
-if len(refPt) == 2:
-	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-	cv2.imshow("ROI", roi)
-
-	hsvRoi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-	print('min H = {}, min S = {}, min V = {}; max H = {}, max S = {}, max V = {}'.format(hsvRoi[:,:,0].min(), hsvRoi[:,:,1].min(), hsvRoi[:,:,2].min(), hsvRoi[:,:,0].max(), hsvRoi[:,:,1].max(), hsvRoi[:,:,2].max()))
- 
-	lower = np.array([hsvRoi[:,:,0].min(), hsvRoi[:,:,1].min(), hsvRoi[:,:,2].min()])
-	upper = np.array([hsvRoi[:,:,0].max(), hsvRoi[:,:,1].max(), hsvRoi[:,:,2].max()])
-
-	image_to_thresh = clone
-	hsv = cv2.cvtColor(image_to_thresh, cv2.COLOR_BGR2HSV)
-
-	kernel = np.ones((3,3),np.uint8)
-	# for red color we need to masks.
-	mask = cv2.inRange(hsv, lower, upper)
-	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-	mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-	cv2.imshow("Mask", mask)
-	cv2.waitKey(0)
-# close all open windows
+# cleanup the camera and close any open windows
+camera.release()
 cv2.destroyAllWindows()
