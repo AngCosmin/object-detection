9fae9ccaa7e78a49f7e5a167e57faa5ce6a7d7b3
Cosmin <cosminzorr@gmail.com>
2018-02-18 15:46:58 +0200

    no msg

--- classes/Ultrasonic.py
+++ classes/Ultrasonic.py
@@ -9,23 +9,27 @@ class Ultrasonic:
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.echo_pin, GPIO.IN)
         GPIO.setup(self.trig_pin, GPIO.OUT)
+        GPIO.output(self.trig_pin, False)
+
 
     def measure(self):
-        GPIO.output(self.trig_pin, False)
         GPIO.output(self.trig_pin, True)
         sleep(0.00001)
         GPIO.output(self.trig_pin, False)
 
         while GPIO.input(self.echo_pin) == 0:
-            print 'Measure 0'
-            pulse_start = time()
+            print 'zero'
+            pass
+
+        pulse_start = time()
         
         while GPIO.input(self.echo_pin) == 1:
-            print 'Measure 1'            
-            pulse_end = time()
+            pass
+
+        pulse_end = time()
 
         pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17150
+        distance = pulse_duration * 17000
         distance = round(distance, 2)
 
         return distance
